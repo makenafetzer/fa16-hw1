6b34dd63450627d40965324e7ee0765a29aba520
Makena Fetzer <mfetzer@berkeley.edu>
2016-09-19 12:39:53 -0700

    Update foobar.rb

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,12 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a.map! {|string| Integer(string) + 2}
+    a.keep_if {|int| int%2 == 0}
+    a.reject! {|int| int > 10}
+    a.uniq!
+    sum = 0
+    a.each {|int| sum += int}
+    sum
   end
 end
-
-
