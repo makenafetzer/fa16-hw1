823989a965d01ca2cb2a6285d3c3ad590f2c0fa4
Makena Fetzer <mfetzer@berkeley.edu>
2016-09-19 12:38:59 -0700

    Update hw1.rb

--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,17 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  (a + b) ** 2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  a.map! {|int| int + 1}
+  a.sort
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  first_name + " " + last_name
 end
 
 def blockin_time(a)
